84e12460a94977df6b70c593380db88669e947a1
EZ <dza9393@mail.ru>
2023-05-06 16:01:02 +0300

    1-5 tasks

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -3,9 +3,7 @@ package org.example;
 import org.example.model.Student;
 import org.example.service.StudentService;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Iterator;
+import java.util.*;
 
 /**
  * Hello world!
@@ -16,10 +14,21 @@ public class App
     public static void main( String[] args )
     {
         StudentService st = new StudentService();
+        //1
         int t =  st.getAgeByName(st.createStudent(), "Fox");
         System.out.println("1. Age = "+ t);
+        //2
         HashSet<String> s = st.getSetWithNames(st.createStudent());
         System.out.println("2. Names = "+ s);
+        //3
+        boolean b = st.defineStudentAgeMoreThan(st.createStudent(),99);
+        System.out.println("3. Boolean decesion= "+ b);
+        //4
+        HashMap<Integer,String>  m = st.transferListToMap(st.createStudent());
+        System.out.println("4. HashMap = " + m);
+        //5
+        HashMap<Integer, List<Student>> n = (HashMap<Integer, List<Student>>) st.transferListToMapWithCollection(st.createStudent());
+        System.out.println("5. HashMap2 = " + n);
 
     }
 }
--- src/main/java/org/example/service/StudentService.java
+++ src/main/java/org/example/service/StudentService.java
@@ -2,9 +2,9 @@ package org.example.service;
 
 import org.example.model.Student;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class StudentService {
 
@@ -13,7 +13,7 @@ public class StudentService {
         ArrayList<Student> students = new ArrayList<Student>();
         Student st1 = new Student(1, "Fox", 23);
         Student st2 = new Student(2, "Tom", 30);
-        Student st3 = new Student(3, "Rim", 55);
+        Student st3 = new Student(3, "Rim", 23);
         Student st4 = new Student(4, "Dim", 44);
         Student st5 = new Student(5, "Fox", 99);
         Student st6 = new Student(6, "Kevin", 11);
@@ -65,8 +65,45 @@ public class StudentService {
 
     //3. Узнать содержит ли список хотя бы 1 участника, у которого возраст больше заданного числа
 
-//    public boolean defineStudentAgeMoreThan (ArrayList<Student> studentList, int age)
-//    {
-//
-//    }
+    public boolean defineStudentAgeMoreThan (ArrayList<Student> studentList, int age)
+    {
+        boolean decesionAnswer = false;
+        Iterator<Student> it = studentList.iterator();
+        while (it.hasNext())
+        {
+            if (age<it.next().getAge())
+            {
+                decesionAnswer=true;
+                return decesionAnswer;
+            }
+
+        }
+        return decesionAnswer;
+    }
+
+
+    //4. Преобразовать список в Map, у которого ключ - уникальный идентификатор, значение - имя
+
+    public HashMap<Integer, String> transferListToMap (ArrayList<Student> arList)
+    {
+        HashMap<Integer, String> newMap = new HashMap<>();
+
+        Iterator<Student> itr = arList.iterator();
+        while (itr.hasNext())
+        {
+            Student st = itr.next();
+            newMap.put(st.getStudentId(),st.getName());
+        }
+        return newMap;
+    }
+
+    //5. преобразовать список в Map, у которого ключ - возраст,
+    // значение - коллекция объектов Student,
+    // которым соответствует возраст, указанный в ключе
+    public Map<Integer, List<Student>> transferListToMapWithCollection (ArrayList<Student> students)
+    {
+        HashMap<Integer,ArrayList<Student>> result = new HashMap<>();
+        return students.stream().collect(Collectors.groupingBy(Student::getAge, Collectors.mapping(Function.identity(), Collectors.toList())));
+
+    }
 }
